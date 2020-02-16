c7ba8307c8545bae60a9e00326cde2ad130ca2aa
Garen Sahagian <g.s.sahagian@gmail.com>
2020-02-16 21:21:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
   counter = 0
   index = nil
-  while array[counter] do
-    if array[counter] == value_to_find
+  while array[counter]>array.length do
+    if array[counter] =! value_to_find
       index=counter
       counter+=1
     else
